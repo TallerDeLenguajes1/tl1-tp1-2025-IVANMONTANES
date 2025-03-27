07203ec5275ee83e59edbe170c3fbdda4bb700ea
ivan <ivanmontanesfilipit@gmail.com>
2025-03-26 23:26:48 -0300

    punto 3/e realizado

--- tp1_1.c
+++ tp1_1.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
 int main(){
-    printf("hola mundo\n");
+    int numero = 12, *puntero = &numero;
+    printf("contenido del puntero %d\n",*puntero);
+    printf("direccion de memoria almacenado por el puntero %p\n",puntero);
+    printf("direccion de memoria de la variable %p\n",&numero);
+    printf("direccion de memoria del puntero: %p\n",&puntero);
+    printf("tamaño de memoria utilizada por la variable numero: %d\n",sizeof(numero));
     return 0;
 }
